8d99dd3d57c725a953a99516a6d6d63641d7faba
dattr09 <datsml0@gmail.com>
2025-03-26 00:58:36 +0700

    done GiaoDien ShopCart

--- obj/Debug/net9.0/WebsiteBanHang.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebsiteBanHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebsiteBanHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a3af2838ed8cba755d77e83970cc4b45e010263")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4da09c0cb642f804c7d2e05ba4b51bd9f21db3ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebsiteBanHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebsiteBanHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
